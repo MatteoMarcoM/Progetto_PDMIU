dd763c015a2724b9cf4cb56d8f925286c3094bb4
Matteo Marco <matteomm00@gmail.com>
2022-12-29 17:36:07 +0100

    added user drawer layout

--- lib/widgets/userDrawer.dart
+++ lib/widgets/userDrawer.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:pdmiu_app_biblioteca/pages/userPages/userDeleteBookPage.dart';
+import 'package:pdmiu_app_biblioteca/pages/userPages/userRenameBookPage.dart';
+import '../models/user.dart';
 import 'package:pdmiu_app_biblioteca/pages/biblioHomePage.dart';
-import 'package:pdmiu_app_biblioteca/pages/loginUserPage.dart';
-import 'package:pdmiu_app_biblioteca/pages/registerUserPage.dart';
-import 'package:pdmiu_app_biblioteca/pages/userListPage.dart';
+import 'package:pdmiu_app_biblioteca/pages/userPages/userAddBookPage.dart';
+import 'package:pdmiu_app_biblioteca/pages/userPages/userBookListPage.dart';
+
+// da fare con current user e provider
+final userMario = User(username: 'mario', password: 'passwordDiMario');
 
 class UserDrawer extends StatelessWidget {
   const UserDrawer({super.key});
@@ -24,33 +29,58 @@ class UserDrawer extends StatelessWidget {
           },
         ),
         ListTile(
-          leading: const Icon(Icons.app_registration),
-          title: const Text("Registra utente"),
-          subtitle: const Text("Registra un utente al servizio"),
+          leading: const Icon(Icons.my_library_books),
+          title: const Text("Lista Libri"),
+          subtitle: const Text("Mostra la lista dei libri"),
           onTap: () {
             Navigator.of(context).pushAndRemoveUntil(
                 MaterialPageRoute(
-                    builder: (context) => const RegisterUserPage()),
+                    builder: (context) => UserBookListPage(user: userMario)),
                 (route) => false);
           },
         ),
         ListTile(
-          leading: const Icon(Icons.login_outlined),
-          title: const Text("Login utente"),
-          subtitle: const Text("Accedi al servizio tramite login"),
+          leading: const Icon(Icons.bookmark_add),
+          title: const Text("Aggiungi Libro"),
+          subtitle: const Text("Aggiungi un libro"),
           onTap: () {
             Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(builder: (context) => const LoginUserPage()),
+                MaterialPageRoute(
+                    builder: (context) => const UserAddBookPage()),
                 (route) => false);
           },
         ),
         ListTile(
-          leading: const Icon(Icons.supervised_user_circle_sharp),
-          title: const Text("Lista utenti"),
-          subtitle: const Text("Lista degli utenti registrati al servizio"),
+          leading: const Icon(Icons.bookmark_remove_rounded),
+          title: const Text("Rimuovi Libro"),
+          subtitle: const Text("Rimuovi un libro"),
           onTap: () {
             Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(builder: (context) => const UserListPage()),
+                MaterialPageRoute(
+                    builder: (context) => const UserDeleteBookPage()),
+                (route) => false);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.change_circle),
+          title: const Text("Rinomina Libro"),
+          subtitle: const Text("Rinomina un libro"),
+          onTap: () {
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(
+                    builder: (context) => const UserRenameBookPage()),
+                (route) => false);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.person_remove_sharp),
+          title: const Text("Cancella account"),
+          subtitle: const Text("Disiscriviti dal servizio"),
+          onTap: () {
+            userMario.deleteUser();
+
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => const BiblioHomePage()),
                 (route) => false);
           },
         ),
